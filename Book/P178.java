4ec13ca41d16479def8cd172431cdf01a07427f1
Oded <obenamotz@gmail.com>
2024-03-20 20:53:41 +0200

    Updated

--- Book/P178.java
+++ Book/P178.java
@@ -3,14 +3,15 @@ public class P178 {
     public static final Hooks hooks = new Hooks();
 
     public static void main(String[] args) {
-        Queue<Integer> open = hooks.buildQueue(1,7);
-        Queue<Integer> closed = hooks.buildQueue(6,9);
+        Queue<Integer> open = hooks.buildQueue(1, 7);
+        Queue<Integer> closed = hooks.buildQueue(6, 9);
         Q9(open, closed);
         System.out.println(open);
     }
-    public static <T> void Q9(Queue<T> open, Queue<T> closed){
-        Queue<T> storage = new Queue<>();
-        while(!open.isEmpty() && !closed.isEmpty()){
+
+    public static void Q9(Queue<Integer> open, Queue<Integer> closed) {
+        Queue<Integer> storage = new Queue<>();
+        while (!open.isEmpty() && !closed.isEmpty()) {
             storage.insert(open.remove());
             storage.insert(closed.remove());
         }
@@ -18,6 +19,7 @@ public class P178 {
         hooks.spill(storage, closed);
         hooks.spill(open, storage);
     }
+
     public static void Q10(Queue<Integer> q1, Queue<Integer> q2) {
         hooks.spill(q1, q2);
     }
